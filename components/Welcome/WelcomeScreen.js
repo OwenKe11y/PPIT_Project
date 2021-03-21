5305fc7dd962a9d3e3bae725d6642f4a63c74f2b
Enda Goggin <g00369903@gmit.ie>
2021-03-21 22:09:48 +0000

    Added recording and playback buttons

--- components/Welcome/WelcomeScreen.js
+++ components/Welcome/WelcomeScreen.js
@@ -1,10 +1,60 @@
-import { ImageBackground, StyleSheet, View, Text } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text, Pressable } from 'react-native';
 import React from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as firebase from 'firebase';
+import { Audio } from 'expo-av'
 
 
 
 export default function WelcomeScreen({ navigation }) {
+  const [recording, setRecording] = React.useState();
+  const [sound, setSound] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync(); 
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+  }
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('../../assets/DBC.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
   return (
     <ImageBackground
       style={styles.background}
@@ -13,9 +63,6 @@ export default function WelcomeScreen({ navigation }) {
         <Text style={styles.title}>Welcome to your Music App</Text>
       </View>
       {/* Button for uploading test */}
-      <TouchableOpacity style={styles.button} onPress={uploadPress}>
-        <Text style={styles.buttonText}>Upload</Text>
-      </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign Up')} >
         <Text style={styles.buttonText}>Sign Up</Text>
       </TouchableOpacity>
@@ -23,6 +70,17 @@ export default function WelcomeScreen({ navigation }) {
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sign In')}>
         <Text style={styles.buttonText}>Sign In</Text>
       </TouchableOpacity>
+
+      <Pressable style={styles.buttonRec} onPress={startRecording}>
+      </Pressable>
+        <Text>Rec</Text>
+      <Pressable style={styles.buttonRec} onPress={stopRecording}>
+        <Text>Stop</Text>
+      </Pressable>
+      <Pressable style={styles.buttonRec} onPress={playSound}>
+        <Text>DBC</Text>
+      </Pressable>
+
     </ImageBackground>
   )
 }
@@ -44,6 +102,18 @@ const styles = StyleSheet.create({
     padding: 14,
     margin: '8%'
   },
+
+  buttonRec: {
+    padding: 14,
+    borderWidth: 2,
+    borderColor: 'white',
+    backgroundColor: '#FF0000',
+    borderRadius: 50,
+    maxWidth: '50%',
+    maxHeight: '15%',
+    color: 'red'
+  },
+
   buttonText: {
     fontSize: 20,
     fontWeight: 'bold',
